3b348f516dc5ae5f1433129ad5ef0a724cd78aca
baegteun <baegteun@gmail.com>
2022-11-03 22:18:23 +0900

    feat :: DMSAPI Template

---
+++ Source/DMSAPI.xctemplate/___VARIABLE_productName___API.swift
@@ -0,0 +1,34 @@
+import DataMappingModule
+import DomainModule
+import ErrorModule
+import Moya
+
+public enum ___VARIABLE_productName___API {
+    
+}
+
+extension ___VARIABLE_productName___API: DmsAPI {
+    public var domain: DmsDomain {
+        <#Domain#>
+    }
+
+    public var urlPath: String {
+        return ""
+    }
+
+    public var method: Moya.Method {
+        return .get
+    }
+
+    public var task: Moya.Task {
+        return .requestPlain
+    }
+
+    public var jwtTokenType: JwtTokenType {
+        .accessToken
+    }
+
+    public var errorMap: [Int: ErrorModule.DmsError] {
+        [:]
+    }
+}
